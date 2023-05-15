427d2dce8cd3da89d622bd2b62befc959d3f2aa7
Karthikeya.Gundumogula <karthikeya200226@gmail.com>
2023-05-15 20:19:03 +0530

    Completed some event handlers in Marketplace and ContentManger

--- src/contract.ts
+++ src/contract.ts
@@ -16,9 +16,37 @@ export function handleSocialTokenLaunched(
     return;
   } else {
     socialtoken.IsLaunched = true;
+    socialtoken.AmountAvailbleforSale = socialtoken.TotalAmountMinted;
+    socialtoken.LaunchingPrice = event.params.price;
+    socialtoken.AmountListedByHolder = socialtoken.TotalAmountMinted;
+    socialtoken.PriceSetByHolder = event.params.price;
+    socialtoken.VideoId = event.params.videoIds;
+    socialtoken.save();
   }
 }
 
-export function handleVideoPublished(event: VideoPublishedEvent): void {}
+export function handleVideoPublished(event: VideoPublishedEvent): void {
+  let video = Video.load(event.params.videoId.toString());
 
-export function handleVideoUnpublished(event: VideoUnpublishedEvent): void {}
+  if (!video) {
+    return;
+  } else {
+    video.Published = true;
+    video.LastPublishedDate = new Date(
+      event.block.timestamp.toI32() * 1000
+    ).toString();
+    video.RoomId = event.params.roomId;
+    video.save();
+  }
+}
+
+export function handleVideoUnpublished(event: VideoUnpublishedEvent): void {
+  let video = Video.load(event.params.videoId.toString());
+
+  if (!video) {
+    return;
+  } else {
+    video.Published = false;
+    video.save();
+  }
+}
--- src/market-place.ts
+++ src/market-place.ts
@@ -7,154 +7,37 @@ import {
   SocialTokenUnlisted as SocialTokenUnlistedEvent,
   VideoListed as VideoListedEvent,
   VideoPurchased as VideoPurchasedEvent,
-  VideoUnlisted as VideoUnlistedEvent
-} from "../generated/MarketPlace/MarketPlace"
-import {
-  RoomListed,
-  RoomPurchased,
-  RoomUnlisted,
-  SocialTokenListed,
-  SocialTokenPurchased,
-  SocialTokenUnlisted,
-  VideoListed,
-  VideoPurchased,
-  VideoUnlisted
-} from "../generated/schema"
+  VideoUnlisted as VideoUnlistedEvent,
+} from "../generated/MarketPlace/MarketPlace";
+import { Room, Video, SocialTokenHolding } from "../generated/schema";
 
 export function handleRoomListed(event: RoomListedEvent): void {
-  let entity = new RoomListed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._id = event.params._id
-  entity._price = event.params._price
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let room = Room.load(event.params._id.toString());
+  if (!room) {
+    return;
+  } else {
+    room.IsListed = true;
+    room.Price = event.params._price;
+    room.save();
+  }
 }
 
-export function handleRoomPurchased(event: RoomPurchasedEvent): void {
-  let entity = new RoomPurchased(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._id = event.params._id
-  entity._buyer = event.params._buyer
-  entity._seller = event.params._seller
-  entity._price = event.params._price
+export function handleRoomPurchased(event: RoomPurchasedEvent): void {}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+export function handleRoomUnlisted(event: RoomUnlistedEvent): void {}
 
-  entity.save()
-}
-
-export function handleRoomUnlisted(event: RoomUnlistedEvent): void {
-  let entity = new RoomUnlisted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._id = event.params._id
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleSocialTokenListed(event: SocialTokenListedEvent): void {
-  let entity = new SocialTokenListed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._id = event.params._id
-  entity._price = event.params._price
-  entity._seller = event.params._seller
-  entity._amount = event.params._amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+export function handleSocialTokenListed(event: SocialTokenListedEvent): void {}
 
 export function handleSocialTokenPurchased(
   event: SocialTokenPurchasedEvent
-): void {
-  let entity = new SocialTokenPurchased(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._id = event.params._id
-  entity._buyer = event.params._buyer
-  entity._seller = event.params._seller
-  entity._price = event.params._price
-  entity._amount = event.params._amount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+): void {}
 
 export function handleSocialTokenUnlisted(
   event: SocialTokenUnlistedEvent
-): void {
-  let entity = new SocialTokenUnlisted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._id = event.params._id
-  entity._amount = event.params._amount
-  entity._seller = event.params._seller
+): void {}
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+export function handleVideoListed(event: VideoListedEvent): void {}
 
-  entity.save()
-}
+export function handleVideoPurchased(event: VideoPurchasedEvent): void {}
 
-export function handleVideoListed(event: VideoListedEvent): void {
-  let entity = new VideoListed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._id = event.params._id
-  entity._price = event.params._price
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleVideoPurchased(event: VideoPurchasedEvent): void {
-  let entity = new VideoPurchased(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._id = event.params._id
-  entity._buyer = event.params._buyer
-  entity._seller = event.params._seller
-  entity._price = event.params._price
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleVideoUnlisted(event: VideoUnlistedEvent): void {
-  let entity = new VideoUnlisted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._id = event.params._id
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+export function handleVideoUnlisted(event: VideoUnlistedEvent): void {}
